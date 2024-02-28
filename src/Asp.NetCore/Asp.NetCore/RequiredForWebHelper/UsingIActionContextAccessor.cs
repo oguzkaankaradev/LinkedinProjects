14ec13e1b3212d2a08d403c94919708dedd2955f
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-02-28 14:25:27 +0800

    IActionContextAccessor

---
+++ src/Asp.NetCore/Asp.NetCore/RequiredForWebHelper/UsingIActionContextAccessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Asp.NetCore.RequiredForWebHelper
+{
+    public class UsingIActionContextAccessor
+    {
+        private readonly IActionContextAccessor _actionContextAccessor;
+
+        public UsingIActionContextAccessor(IActionContextAccessor actionContextAccessor)
+        {
+            _actionContextAccessor = actionContextAccessor;
+        }
+
+        public void MyService()
+        {
+            ActionContext actionContext = _actionContextAccessor.ActionContext;
+        }
+    }
+
+   
+}
