1128122a1b91f5b05af4758f9c90d552f1e02e47
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-03-01 08:59:17 +0800

    UsingIHttpContextAccessorController

---
+++ src/Asp.NetCore/Asp.NetCore/RequiredForWebHelper/UsingIHttpContextAccessor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Asp.NetCore.RequiredForWebHelper
+{
+    public class UsingIHttpContextAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UsingIHttpContextAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            // HttpContext'a erişim sağlamak için Context kullanılır
+            var userName = Context.User.Identity.Name;
+
+            // Diğer HttpContext özelliklerine erişim
+
+            // Örnek: Bağlı olan istemcilere mesaj gönderme
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
+        }
+    }
+}
---
+++ src/WebAPI/Asp.NetCore.WebAPI/Controllers/UsingIHttpContextAccessor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore.RequiredForWebHelper
+{
+    public class UsingIHttpContextAccessorController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UsingIHttpContextAccessorController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult GetUserInfo()
+        {
+            HttpContext httpContext = _httpContextAccessor.HttpContext;
+            
+            string userName = httpContext.User.Identity.Name;
+
+            string message = httpContext.Request.PathBase;
+
+            return Ok($"Current user: {userName}");
+
+        }
+
+        //Middleware
+        public class HttpContextAccessorMiddleware
+        {
+            public Task InvokeAsync(HttpContext context)
+            {
+                //
+                return Task.CompletedTask;
+            }
+        }
+
+        //Access HttpContext from SignalR
+        
+
+    }
+}
