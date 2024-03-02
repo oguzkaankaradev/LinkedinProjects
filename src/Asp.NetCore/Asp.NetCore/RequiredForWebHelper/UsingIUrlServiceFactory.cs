de41da65e5a787f2db68e70479f1e8a60675d417
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-03-02 09:01:26 +0800

    CreatedUsingIUrlServiceFactory

---
+++ src/Asp.NetCore/Asp.NetCore/RequiredForWebHelper/UsingIUrlServiceFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore.RequiredForWebHelper
+{
+    public class UsingIUrlServiceFactory
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public UsingIUrlServiceFactory(IUrlHelperFactory urlHelperFactory, ActionContext actionContext)
+        {
+            _urlHelper = urlHelperFactory.GetUrlHelper(actionContext);
+        }
+
+        public string GetUrl()
+        {
+            // Perform URL creation or management using the _urlHelper object
+            var url = _urlHelper.Action("ActionName", "ControllerName");
+
+            // ...
+
+            return url;
+        }
+    }
+}
---
+++ src/WebAPI/Asp.NetCore.WebAPI/Controllers/UsingIUrlHelperFactoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore.WebAPI.Controllers
+{
+    public class UsingIUrlHelperFactoryController : Controller
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public UsingIUrlHelperFactoryController(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+        public IActionResult Index()
+        {
+            // Perform URL creation or management using the _urlHelper object
+            var url = _urlHelper.Action("ActionName", "ControllerName");
+
+            // .. 
+
+            return View();
+        }
+    }
+}
