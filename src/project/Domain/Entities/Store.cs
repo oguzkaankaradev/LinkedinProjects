3ed2eada14bde04a65b31d0a36ea8327d55d42ca
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-03-04 11:42:59 +0800

    AddedStoreToDomainLayer

---
+++ src/project/Domain/Entities/Store.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string DefaultMetaKeywords { get; set; }
+        public string DefaultMetaDescription { get; set; }
+        public string DefaultTitle { get; set; }
+        public string HomepageTitle { get; set; }
+        public string HomepageDescription { get; set; }
+        public string Url { get; set; }
+        public bool SslEnabled { get; set; }
+        public string Hosts { get; set; }
+        public int DefaultLanguageId { get; set; }
+        public int DisplayOrder { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyPhoneNumber { get; set; }
+        public string CompanyVat { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
