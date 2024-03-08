33475e1c10c5c6ccc6721c22e89c60eef591b33e
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-03-08 16:47:52 +0800

    CreatedIStoreContextToApplicationContext

---
+++ src/project/Persistance/Contexts/StoreContext/IStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Contexts.StoreContext
+{
+    public interface IStoreContext
+    {
+        Task<Store> GetCurrentStoreAsync();
+        Store GetCurrentStore();
+        Task<int> GetActiveStoreScopeConfigurationAsync();
+    }
+}
